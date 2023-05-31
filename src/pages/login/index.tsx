2f6c995e7f281050de7cc6ce4765e8d9d62ef3ed
jia-wei-00 <wwwbingo6@gmail.com>
2023-05-31 17:59:43 +0800

    update

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,64 +1,19 @@
 import React from "react";
 import "../../styles/pages/login.scss";
-import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  FormControl,
-  FormHelperText,
-  IconButton,
-  Input,
-  InputAdornment,
-  InputLabel,
-} from "@mui/material";
-import {
-  EmailRounded,
-  Lock,
-  Visibility,
-  VisibilityOff,
-} from "@mui/icons-material";
+import { Button, Card, CardContent } from "@mui/material";
 import { motion } from "framer-motion";
 import { HomeParticle } from "../../components";
-import { authStore } from "../../stores";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { registerSchema, loginSchema } from "../../schemas/login-page-schemas";
-import { InputData } from "../../types/form";
 import ResetPassword from "./reset-password";
+import RegisterForm from "./register-form";
+import LoginForm from "./login-form";
 
 const LoginPage: React.FC = () => {
-  const [showPassword, setShowPassword] = React.useState<boolean>(false);
-  const [showRepeatPassword, setShowRepeatPassword] =
-    React.useState<boolean>(false);
   const [active, setActive] = React.useState<string>("login");
   const [openResetModal, setOpenResetModal] = React.useState<boolean>(false);
 
-  const {
-    register,
-    formState: { errors, isSubmitSuccessful },
-    reset,
-    handleSubmit,
-  } = useForm<InputData>({
-    resolver: zodResolver(active === "login" ? loginSchema : registerSchema),
-  });
-
-  React.useEffect(() => {
-    if (isSubmitSuccessful) {
-      reset();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isSubmitSuccessful]);
-
-  const onSubmitHandler: SubmitHandler<InputData> = (values) => {
-    active === "login"
-      ? authStore.signIn(values.email, values.password)
-      : authStore.signUp(values.email, values.passwordConfirm!);
-  };
-
   return (
     <div className="wrapper">
-      <motion.div initial={{ y: 400 }} animate={{ y: 0 }}>
+      <motion.div initial={{ y: 400 }} animate={{ y: 0 }} className="form-body">
         <Card className="card">
           <CardContent>
             <motion.div className="btn-group">
@@ -69,126 +24,24 @@ const LoginPage: React.FC = () => {
                 className="indicator"
               />
             </motion.div>
-            <motion.form
-              className="mt-10"
-              onSubmit={handleSubmit(onSubmitHandler)}
+            <motion.div
+              animate={
+                active === "login"
+                  ? { height: "auto" }
+                  : { height: 0, opacity: 0 }
+              }
             >
-              {/* Email Input */}
-              <Box
-                className={`${
-                  !!errors["email"] ? "flex-center" : "flex-end"
-                } box mt-10`}
-              >
-                <EmailRounded className="icon" />
-                <FormControl variant="standard" className="form-control">
-                  <InputLabel
-                    error={!!errors["email"]}
-                    htmlFor="standard-email"
-                  >
-                    Email
-                  </InputLabel>
-                  <Input
-                    error={!!errors["email"]}
-                    type="email"
-                    {...register("email")}
-                  />
-                  <FormHelperText error={!!errors["email"]}>
-                    {errors["email"] ? errors["email"].message : ""}
-                  </FormHelperText>
-                </FormControl>
-              </Box>
-
-              {/* Password Input */}
-              <Box
-                className={`${
-                  !!errors["password"] ? "flex-center" : "flex-end"
-                } box`}
-              >
-                <Lock className="icon" />
-                <FormControl variant="standard">
-                  <InputLabel
-                    error={!!errors["password"]}
-                    htmlFor="standard-adornment-password"
-                  >
-                    Password
-                  </InputLabel>
-                  <Input
-                    type={showPassword ? "text" : "password"}
-                    error={!!errors["password"]}
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={() => setShowPassword((show) => !show)}
-                        >
-                          {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      </InputAdornment>
-                    }
-                    {...register("password")}
-                  />
-                  <FormHelperText error={!!errors["password"]}>
-                    {errors["password"] ? errors["password"].message : ""}
-                  </FormHelperText>
-                </FormControl>
-              </Box>
-
-              {/* Repeat Password Input */}
-              <motion.div
-                initial={{ height: 0, opacity: 0 }}
-                animate={
-                  active === "register"
-                    ? { height: "auto", opacity: 1 }
-                    : { marginTop: -40, height: 0, opacity: 0 }
-                }
-              >
-                <Box
-                  className={`${
-                    !!errors["passwordConfirm"] ? "flex-center" : "flex-end"
-                  } box`}
-                >
-                  <Lock className="icon" />
-                  <FormControl variant="standard">
-                    <InputLabel
-                      error={!!errors["passwordConfirm"]}
-                      htmlFor="standard-adornment-password"
-                    >
-                      Repeat Password
-                    </InputLabel>
-                    <Input
-                      type={showRepeatPassword ? "text" : "password"}
-                      error={!!errors["passwordConfirm"]}
-                      endAdornment={
-                        <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={() =>
-                              setShowRepeatPassword((show) => !show)
-                            }
-                          >
-                            {showRepeatPassword ? (
-                              <VisibilityOff />
-                            ) : (
-                              <Visibility />
-                            )}
-                          </IconButton>
-                        </InputAdornment>
-                      }
-                      {...register("passwordConfirm")}
-                    />
-                    <FormHelperText error={!!errors["passwordConfirm"]}>
-                      {errors["passwordConfirm"]
-                        ? errors["passwordConfirm"].message
-                        : ""}
-                    </FormHelperText>
-                  </FormControl>
-                </Box>
-              </motion.div>
-
-              <Button type="submit" variant="contained" className="mt-10">
-                {active === "login" ? "LOGIN" : "REGISTER"}
-              </Button>
-            </motion.form>
+              <LoginForm />
+            </motion.div>
+            <motion.div
+              animate={
+                active === "register"
+                  ? { height: "auto", marginTop: "-15px" }
+                  : { height: 0, opacity: 0 }
+              }
+            >
+              <RegisterForm />
+            </motion.div>
           </CardContent>
         </Card>
       </motion.div>
--- src/pages/login/login-form.tsx
+++ src/pages/login/login-form.tsx
@@ -1,7 +1,105 @@
 import React from "react";
+import { authStore } from "../../stores";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "../../schemas/login-page-schemas";
+import { InputData } from "../../types/form";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { motion } from "framer-motion";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+} from "@mui/material";
+import {
+  EmailRounded,
+  Lock,
+  Visibility,
+  VisibilityOff,
+} from "@mui/icons-material";
 
-const loginForm: React.FC = () => {
-  return <div>loginForm</div>;
+const LoginForm: React.FC = () => {
+  const [showPassword, setShowPassword] = React.useState<boolean>(false);
+
+  const {
+    register,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+    handleSubmit,
+  } = useForm<InputData>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmitHandler: SubmitHandler<InputData> = (values) => {
+    authStore.signIn(values.email, values.password);
+  };
+
+  return (
+    <motion.form className="mt-10" onSubmit={handleSubmit(onSubmitHandler)}>
+      {/* Email Input */}
+      <Box
+        className={`${
+          !!errors["email"] ? "flex-center" : "flex-end"
+        } box mt-10`}
+      >
+        <EmailRounded className="icon" />
+        <FormControl variant="standard" className="form-control">
+          <InputLabel error={!!errors["email"]} htmlFor="standard-email">
+            Email
+          </InputLabel>
+          <Input
+            error={!!errors["email"]}
+            type="email"
+            {...register("email")}
+          />
+          <FormHelperText error={!!errors["email"]}>
+            {errors["email"] ? errors["email"].message : ""}
+          </FormHelperText>
+        </FormControl>
+      </Box>
+
+      {/* Password Input */}
+      <Box
+        className={`${!!errors["password"] ? "flex-center" : "flex-end"} box`}
+      >
+        <Lock className="icon" />
+        <FormControl variant="standard">
+          <InputLabel
+            error={!!errors["password"]}
+            htmlFor="standard-adornment-password"
+          >
+            Password
+          </InputLabel>
+          <Input
+            type={showPassword ? "text" : "password"}
+            error={!!errors["password"]}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => setShowPassword((show) => !show)}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            {...register("password")}
+          />
+          <FormHelperText error={!!errors["password"]}>
+            {errors["password"] ? errors["password"].message : ""}
+          </FormHelperText>
+        </FormControl>
+      </Box>
+
+      <Button type="submit" variant="contained" className="mt-10">
+        REGISTER
+      </Button>
+    </motion.form>
+  );
 };
 
-export default loginForm;
+export default LoginForm;
