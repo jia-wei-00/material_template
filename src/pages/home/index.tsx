0b5022df02433627bb72c3a198965a615930d54c
jia-wei-00 <wwwbingo6@gmail.com>
2023-06-07 00:48:33 +0800

    add websocket

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { subscribeTicks } from "../../websocket";
 
 const HomePage: React.FC = () => {
+  React.useEffect(() => {
+    subscribeTicks();
+  }, []);
+
   return <div>home</div>;
 };
 
